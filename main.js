47de0cd61c8f222a75d0194a9ea4bba3e55d2179
LorenzoTorelli <lorenzo.torelli98@gmail.com>
2021-11-08 17:47:18 +0100

    v2

--- main.js
+++ main.js
@@ -1,14 +1,28 @@
+// const apri = document.querySelector(".fas.fa-bars");
+
+// apri.addEventListener("click", function (){ 
+//     document.querySelector(".hamburger-menu").style.display = "block";}
+// )
+
+// const chiudi = document.querySelector(".fas.fa-times");
+
+// chiudi.addEventListener("click", function (){ 
+//     document.querySelector(".hamburger-menu").style.display = "none";
+//     }
+// )
+
+
 const apri = document.querySelector(".fas.fa-bars");
 
 apri.addEventListener("click", function (){ 
-    document.querySelector(".hamburger-menu").style.display = "block";}
+    let ham = document.querySelector(".hamburger-menu");
+    ham.className = ham.classList + " active";
+    }
 )
-
 const chiudi = document.querySelector(".fas.fa-times");
 
 chiudi.addEventListener("click", function (){ 
-    document.querySelector(".hamburger-menu").style.display = "none";
+    let ham = document.querySelector(".hamburger-menu");
+    ham.classList.remove("active");
     }
 )
-
-
