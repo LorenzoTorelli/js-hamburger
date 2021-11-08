eda34b9405f78abe1c6f2e725350c37726dced31
LorenzoTorelli <lorenzo.torelli98@gmail.com>
2021-11-08 17:00:22 +0100

    versione alternativa

--- main.js
+++ main.js
@@ -1,13 +1,32 @@
-const apri = document.querySelector(".fas.fa-bars");
+// const apri = document.querySelector(".fas.fa-bars");
 
-apri.addEventListener("click", function (){ 
-    document.querySelector(".hamburger-menu").style.display = "block";}
+// apri.addEventListener("click", function (){ 
+//     document.querySelector(".hamburger-menu").style.display = "block";}
+// )
+
+// const chiudi = document.querySelector(".fas.fa-times");
+
+// chiudi.addEventListener("click", function (){ 
+//     document.querySelector(".hamburger-menu").style.display = "none";
+//     }
+// )
+
+
+
+// seconda opzione 
+
+
+const apri = document.querySelector(".fas");
+
+apri.addEventListener("click", function() {
+    // alert();
+    let ham = document.querySelector(".hamburger-menu");
+    if (ham.style.display == "block") {
+        ham.style.display = "none";
+        } 
+    else {
+        ham.style.display = "block";
+        }
+    }    
 )
-
-const chiudi = document.querySelector(".fas.fa-times");
-
-chiudi.addEventListener("click", function (){ 
-    document.querySelector(".hamburger-menu").style.display = "none";
-    }
-)
-
+  
